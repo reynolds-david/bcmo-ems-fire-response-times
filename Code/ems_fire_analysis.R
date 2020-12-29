a166c3bbb581bee18001d66e1ab901d3bd858fb6
David Reynolds <davidreynolds@mail.missouri.edu>
2020-12-29 12:25:25 -0600

    Getting things set up

--- Code/ems_fire_analysis.R
+++ Code/ems_fire_analysis.R
@@ -278,7 +278,7 @@ enroute_arrive_line
 enroute_arrive_aov <- aov(Enroute_Arrive ~ Year, data = ems_fire)
 summary(enroute_arrive_aov)
 
-#Line plot for Call_Arrive
+# Line plot for Call_Arrive
 call_arrive_year <- ems_fire %>% 
   select(Year, Call_Arrive) %>% 
   group_by(Year) %>% 
